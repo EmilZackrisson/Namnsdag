b09e7a6614c81a35f271637cb3c7a54fec5cd483
Emil Zackrisson <emil.zackrisson@gmail.com>
2019-04-10 14:00:33 +0200

    added exit function

--- src/com/Zacke/Main.java
+++ src/com/Zacke/Main.java
@@ -24,6 +24,9 @@ public class Main {
             if (namnEllerDagInput.equals("namn")) {
                 checkDayFromName(namnEllerDagInput);
             }
+            if (namnEllerDagInput.equals("exit")) {
+                System.exit(0);
+            }
         }
     }
 
